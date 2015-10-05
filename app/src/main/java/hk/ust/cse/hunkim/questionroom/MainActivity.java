8ad314b1ed533e03ab4abb462f1fe4dd3ea790c4
Shu Chuen Tam <shuchuen109@gmail.com>
2015-10-05 22:48:03 +0800

    update the firebase URL

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/MainActivity.java
@@ -25,7 +25,7 @@ import hk.ust.cse.hunkim.questionroom.question.Question;
 public class MainActivity extends ListActivity {
 
     // TODO: change this to your own Firebase URL
-    private static final String FIREBASE_URL = "https://ccwfirebase.firebaseio.com/";
+    private static final String FIREBASE_URL = "https://codeforfood.firebaseio.com/rooms/";
 
     private String roomName;
     private Firebase mFirebaseRef;
